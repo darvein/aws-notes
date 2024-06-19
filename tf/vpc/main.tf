228f4013fc743b09a078af2385759325ab735b7d
Dennis Ruiz <darvein@gmail.com>
2024-06-19 13:11:20 -0400

    Updating notes

--- tf/vpc/main.tf
+++
@@ -1,4 +0,0 @@
-module "vpc" {
-  source = "./vpc-mod"
-  cidr_block = "10.0.0.0/16"
-}
--- tf/vpc/vpc-mod/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "this" {
-  cidr_block = var.cidr_block
-  enable_dns_support = true
-  enable_dns_hostnames = false
-  tags = merge({}, { "Name" = "thevpc" })
-} 
